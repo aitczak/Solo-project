ca979df26f0c67a452878d6db14bcb4d744fcf05
Ali McNamara <allisonrm95@gmail.com>
2024-07-31 16:49:36 -0400

    added formatting to log and entries

--- src/components/logDisplay.js
+++ src/components/logDisplay.js
@@ -13,10 +13,11 @@ const Log = ({
   handleNameInputChange,
 }) => {
   // put render logic here
+
   const LogEntries = entries.map((entryObj, index) => {
     return (
       <p
-        className='logEntries'
+        className='logEntry'
         id={index}
       >{`${entryObj.name} read ${entryObj.pages} pages of ${entryObj.book} on ${entryObj.date}!`}</p>
     );
@@ -26,43 +27,42 @@ const Log = ({
     <div>
       <div id='log'>
         <p id='logHeader'>Look what we've been reading!</p>
-        {LogEntries}
-        <div id='logInputs'>
-          <form onSubmit={handleLogSubmit}>
-            <input
-              className='studentName'
-              type='text'
-              placeholder='Name'
-              value={studentName}
-              onChange={handleNameInputChange}
-            />
-            <input
-              className='numberOfPagesInput'
-              type='text'
-              placeholder='Number of pages'
-              value={newPages}
-              onChange={handleLogInputChange}
-            />
-            <input
-              className='bookTitleInput'
-              type='text'
-              placeholder='Book title'
-              value={bookTitleEntry}
-              onChange={handleBookInputChange}
-            />
-            <input
-              className='dateInput'
-              type='text'
-              placeholder='date'
-              value={dateEntry}
-              onChange={handleDateInputChange}
-            />
-            <button type='submit'>Submit</button>
-          </form>
-        </div>
+        <div id='logEntries'>{LogEntries}</div>
+        <form id='logInputs' onSubmit={handleLogSubmit}>
+          <input
+            className='studentName'
+            type='text'
+            placeholder='Name'
+            value={studentName}
+            onChange={handleNameInputChange}
+          />
+          <input
+            className='numberOfPagesInput'
+            type='text'
+            placeholder='Number of pages'
+            value={newPages}
+            onChange={handleLogInputChange}
+          />
+          <input
+            className='bookTitleInput'
+            type='text'
+            placeholder='Book title'
+            value={bookTitleEntry}
+            onChange={handleBookInputChange}
+          />
+          <input
+            className='dateInput'
+            type='text'
+            placeholder='date'
+            value={dateEntry}
+            onChange={handleDateInputChange}
+          />
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     </div>
   );
 };
 
 export default Log;
+//    const colors = ['#6accf6', '#f6d56a']
